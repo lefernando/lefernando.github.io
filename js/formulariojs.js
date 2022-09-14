a6d7a6ceb23a4b9322b9adf72724acea3d8c5c95
Fernando Lemos <105528069+lefernando@users.noreply.github.com>
2022-09-14 16:54:40 -0300

    Add files via upload

--- js/formulariojs.js
+++ js/formulariojs.js
@@ -1,53 +1,17 @@
 
-
-
-
-var nome = document.getElementById('nome');
-var email = document.getElementById('email');
-var senha = document.getElementById('senha');
-var telefone = document.getElementById('telefone');
-
-email.addEventListener('focus',()=>{
-        email.borderColor="#4A5F6A"
-});
-s
-email.addEventListener('blur',()=>{
-    email.borderColor="#ccc"
-});
-
-senha.addEventListener('focus',()=>{
-    senha.borderColor="#4A5F6A"
-});
-
-senha.addEventListener('blur',()=>{
-senha.borderColor="#ccc"
-});
-
-telefone.addEventListener('focus',()=>{
-    telefone.borderColor="#4A5F6A"
-});
-
-telefone.addEventListener('blur',()=>{
-    telefone.borderColor="#ccc"
-});
-
-nome.addEventListener('focus',()=>{
-    nome.borderColor="#4A5F6A"
-});
-
-nome.addEventListener('blur',()=>{
-    nome.borderColor="#ccc"
-}); 
-
-            document.writeln(nome)
-       
-            document.writeln(email)
-      
-            document.writeln(telefone)
-            
-function reset(LIMPAR){
-}
-
 function enviar(){
+
+    var envio = new Object();
+    envio.Nome = document.getElementById("nome").innerHTML;
+    envio.Telefone = document.getElementById("telefone").innerHTML;
+    envio.Email = document.getElementById("email").innerHTML;
+
+            innerHTML = envio.Nome;
+            innerHTML =  envio.Telefone;
+            innerHTML =  envio.Email;
+            
+}            
+
+function reset(LIMPAR){}
+
     
-}
